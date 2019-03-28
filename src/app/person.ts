07c794dfbd061e593a2fc8750e1cbf2521a66cbd
olexiychebotkevich <olexiychebotkevich@A33COMP4>
2019-03-28 17:48:24 +0200

    new changes

--- src/app/person.ts
+++ src/app/person.ts
@@ -1,2 +1,9 @@
 export class Person {
+    public Name:string;
+    public Surname:string;
+    public NickName:string;
+    public Phone:string;
+    public Avatar:string;
+    public Birthday:Date;
+    public Status:boolean;
 }
--- src/app/phone-book/phone-book.component.ts
+++ src/app/phone-book/phone-book.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Person } from '../person';
+import { PhoneBookService } from '../phone-book.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-phone-book',
@@ -6,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./phone-book.component.scss']
 })
 export class PhoneBookComponent implements OnInit {
-
-  constructor() { }
+  allPeople: Observable<Person[]>;
+  constructor(private formbulider: FormBuilder, private wS: PhoneBookService) { }
 
   ngOnInit() {
+    this.loadAllPeople();
+  }
+
+
+  loadAllPeople() {
+    this.allPeople = this.wS.getAllPeople();
   }
 
 }
